e71ffb10d645aa54edff7cb2fc606c81b1732ecb
Graham King <graham@gkgk.org>
2012-01-17 20:31:22 -0800

    Started work on a python client. Goal is to use curses

---
+++ hjoinpy/hjoin.py
@@ -0,0 +1,58 @@
+"""Connect to hatcogd (start it if necessary), join an IRC channel,
+and provide a user interface to it.
+"""
+
+import logging
+import sys
+import os.path
+
+VERSION    = "hatcog v0.3 (github.com/grahamking/hatcog)"
+DEFAULT_CONFIG = "/.hatcogrc"
+LOG_DIR = "/.hatcog/"
+
+DAEMON_ADDR        = "127.0.0.1:8790"
+
+CMD_START_DAEMON = "start-stop-daemon --start --background --exec /usr/local/bin/hatcogd"
+CMD_STOP_DAEMON  = "start-stop-daemon --stop --exec /usr/local/bin/hatcogd"
+
+ONE_SECOND_NS = 1000 * 1000 * 1000
+RPL_NAMREPLY       = "353"
+RPL_TOPIC          = "332"
+ERR_UNKNOWNCOMMAND = "421"
+CHANNEL_CMDS       = "PRIVMSG, ACTION, PART, JOIN, " + RPL_NAMREPLY
+
+USAGE         = """
+Usage: hjoin [channel|-private=nick]
+Note there's no # in front of the channel
+Examples:
+ 1. Join channel test: go-join test
+ 2. Listen for private (/query) message from bob: go-join -private=bob
+"""
+
+def main(argv=None):
+    if not argv:
+        argv = sys.argv
+
+    home = os.path.expanduser('~')
+    log_filename = home + LOG_DIR + "clientpy.log"
+    print("%s logging to %s" % (VERSION, log_filename))
+    logging.basicConfig(filename=log_filename)
+
+    if len(argv) != 2:
+        print(USAGE)
+        return 1
+
+    arg = sys.argv[1]
+    if arg == "--stop":
+        print("Closing all connections")
+        stop_daemon()
+        return 0
+
+
+def stop_daemon():
+    print("stop_daemon: TODO")
+    pass
+
+
+if __name__ == '__main__':
+    sys.exit(main())
