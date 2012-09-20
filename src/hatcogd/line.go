grahamking/hatcog

commit bc8ee424562c41d19c133ad90344f68bd6a258b4

Tweak for /msg support

src/hatcogd/line.go
@@ -111,8 +111,8 @@ func ParseLine(data string) (*Line, error) {
 
 	if len(channel) == 0 {
 		if command == "PRIVMSG" {
-			// A /query message, fake the user as the channel
-			channel = user
+			// A /query or /msg message, channel is first arg
+			channel = args[0]
 		} else if command == "JOIN" {
 			// JOIN commands say which channel in content part of msg
 			channel = trailing

commit b660984e9d2ead4832addd581d9199a69d942cdd

Fixed regression introduced in bc8ee424, which broke private messages

src/hatcogd/line.go
@@ -112,8 +112,8 @@ func ParseLine(data string) (*Line, error) {
 	if len(channel) == 0 {
 
 		if command == "PRIVMSG" {
-			// A /query or /msg message, channel is first arg
-			channel = args[0]
+			// A /query or /msg message, fake channel as user so we can reply easily
+			channel = user
 
 		} else if command == "JOIN" {
 			// JOIN commands say which channel in content part of msg